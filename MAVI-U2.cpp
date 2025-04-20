f41531fd8862a19ba8e90e4bc2b7a7bbbc5091b8
Alamada Andrés <almada-andres@hotmail.com>
2025-04-20 17:48:15 -0300

    Simulacion Fisica
    Simulacion en consola solamente.

--- MAVI-U2.cpp
+++ MAVI-U2.cpp
@@ -1,8 +1,34 @@
-#include <SFML/Graphics.hpp>
 #include <box2d/box2d.h>
+#include <iostream>
 
-int main() {
+int main()
+{
+    b2World world(b2Vec2(0.0f, -10.0f)); // Mundo con gravedad
 
+    // Piso fijo
+    b2Body* piso = world.CreateBody(new b2BodyDef());
+    b2PolygonShape formaPiso;
+    formaPiso.SetAsBox(50, 10);
+    piso->CreateFixture(&formaPiso, 0.0f);
+
+    // Bloque que cae
+    b2BodyDef defBloque;
+    defBloque.type = b2_dynamicBody;
+    defBloque.position.Set(0, 20);
+    b2Body* bloque = world.CreateBody(&defBloque);
+
+    b2PolygonShape formaBloque;
+    formaBloque.SetAsBox(1, 1);
+    bloque->CreateFixture(&formaBloque, 1.0f);
+
+    //Tiempo de simulacion
+
+    for (int i = 0; i < 60; i++)
+    {
+        world.Step(1.0f / 60.0f, 6, 2);
+        auto pos = bloque->GetPosition();
+        std::cout << "y = " << pos.y << std::endl;
+    }
 
     return 0;
 }
